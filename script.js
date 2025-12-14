68e49b375667eceb6b42c23c88e7c7de4c926d51
hasnainasif0761 <hasnainasif07610761@gmail.com>
2025-12-14 16:38:13 +0500

    'Second-Updation'

--- script.js
+++ script.js
@@ -2,14 +2,8 @@ alert('Welcome to My Medical Reminder App!');
 
 var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-var date = new Date();
-var hours = date.getHours();
 
-var hours12 = hours % 12 || 12;
-var minute = date.getMinutes();
-var AmPm = hours >= 12 ? 'PM' : 'AM';
-var second = date.getSeconds();
-console.log(hours12+":"+minute+" "+AmPm+" "+second);
+
 
 
 
