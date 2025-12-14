26a53d6f78882a26da8eb7a8c74c27b0e08ca14d
hasnainasif0761 <hasnainasif07610761@gmail.com>
2025-12-14 16:41:27 +0500

    'Third-Updation'

--- script.js
+++ script.js
@@ -2,7 +2,30 @@ alert('Welcome to My Medical Reminder App!');
 
 var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
+var medicine = [
+    {
+        name : 'Aspirin',
+        quantity : "2 tablets",
+        hour : 9,
+        minte : 0,
+        period : 'AM',
+    },
+    {
+        name : 'Panadol',
+        quantity : "1 tablet",
+        hour : 2,
+        minte : 30,
+        period : 'PM',
+    },
+    {
+        name : 'Disprin',
+        quantity : "1 tablet",
+        hour : 8,
+        minte : 15,
+        period : 'AM',
+    }
 
+]
 
 
 
